ffd44f433822f9128c7a5ac8364747bca1a8aa55
Nohelia Brito <britonohe0418@gmail.com>
2021-06-24 16:25:43 -0500

    Ejercicio 12

---
+++ ejercicio12.py
@@ -0,0 +1,15 @@
+""" Elabore pseudocódigo para el caso en que se desean escribir los números del 1 al 100.
+"""
+
+class Ejercicio12:
+    def __init__(self):
+        pass
+
+    def numero(self):
+        i = 1
+        while i <= 100:
+            print(i)
+            i = i + 1
+
+escribir = Ejercicio12()
+escribir.numero()
